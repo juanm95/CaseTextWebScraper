4c19289155c9a2676e584279f39346328bd4e107
juanm95 <juanm95@stanford.edu>
2016-09-05 20:27:54 -0700

    Added Server

---
+++ lawgridServer.py
@@ -0,0 +1,48 @@
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import SocketServer
+import json
+import random
+
+class S(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        f = open("index.html", "r")
+        self.wfile.write(f.read())
+
+    def do_HEAD(self):
+        self._set_headers()
+
+    def do_POST(self):
+        self._set_headers()
+        self.data_string = self.rfile.read(int(self.headers['Content-Length']))
+
+        self.send_response(200)
+        self.end_headers()
+
+        data = json.loads(self.data_string)
+        with open("batch5.json", "a") as outfile:
+            json.dump(data, outfile)
+            outfile.write(",\n")
+        print "{}".format(data)
+        return
+
+
+def run(server_class=HTTPServer, handler_class=S, port=80):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print 'Starting httpd...'
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    from sys import argv
+
+if len(argv) == 2:
+    run(port=int(argv[1]))
+else:
+    run()
